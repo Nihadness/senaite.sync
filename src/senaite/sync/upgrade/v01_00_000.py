a87598f625f9f978b1b37ca541e6c4a60575e08c
Nihadness <1992.nihad@gmail.com>
2018-04-26 11:45:03 +0200

    Fix AR's in the wrong state.

--- src/senaite/sync/upgrade/v01_00_000.py
+++ src/senaite/sync/upgrade/v01_00_000.py
@@ -7,7 +7,9 @@
 from Acquisition import aq_inner
 from Acquisition import aq_parent
 from bika.lims import api
+from bika.lims.catalog import CATALOG_ANALYSIS_REQUEST_LISTING
 from bika.lims import logger
+from bika.lims.utils import changeWorkflowState
 from bika.lims.upgrade import upgradestep
 
 version = '1.0.0'
@@ -16,6 +18,7 @@ profile = 'profile-{senaite.sync}:default'
 
 @upgradestep('senaite.sync', version)
 def upgrade(tool):
+
     portal = aq_parent(aq_inner(tool))
     fields_to_update = ['expirationDate', 'effectiveDate']
     types = ['Doctor', 'Instrument', 'Calculation',
@@ -33,4 +36,21 @@ def upgrade(tool):
             field.set(obj, None)
         obj.reindexObject()
         logger.info('Done: {}'.format(obj))
+
+    logger.info("*** Permissions fixed! ***")
+
+    ar_c = api.get_tool(CATALOG_ANALYSIS_REQUEST_LISTING, portal)
+    ars = ar_c(review_state='to_be_verified')
+    logger("Walking through {} AR's in 'to_be_verified' state".format(len(ars)))
+    for ar in ars:
+        obj = ar.getObject()
+        ans = obj.getAnalyses()
+        for an in ans:
+            if an.review_state == 'sample_received':
+                logger.info("Fixing review_state of {} ".format(repr(obj)))
+                changeWorkflowState(obj,'bika_ar_workflow', "sample_received")
+                obj.reindexObject()
+                break
+
+    logger.info("*** AR states fixed! ***")
     return True
